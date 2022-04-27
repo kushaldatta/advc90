7e90fd81cdb94ac5859764893f7bfdad8ec5290a
kushaldatta <96032544+kushaldatta@users.noreply.github.com>
2022-04-27 19:26:29 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,14 @@
+var player1_name = localStorage.getItem("player1_name");
+var player2_name = localStorage.getItem("player2_name");
+var player1_score = 0;
+var player2_score = 0;
+var question_turn = "player1";
+var answer_turn = "player2";
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player1_name").style.color="yellow";
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;  
+document.getElementById("player2_name").style.color="yellow";
